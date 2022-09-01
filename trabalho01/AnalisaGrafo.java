bad84a918cf6ab3534c2007f95200de259af806f
zenialexandre <zenialexandre123@gmail.com>
2022-09-01 11:31:22 -0300

    starting to count the edges

--- trabalho01/AnalisaGrafo.java
+++ trabalho01/AnalisaGrafo.java
@@ -1,16 +1,21 @@
+/**
+* @author Alexandre Zeni
+*/
 package trabalho01;
 
 import java.lang.reflect.Array;
 
 public class AnalisaGrafo {
-    public String tipoDoGrafo(int[][] matrizAdjacencia) { // onde 1 = tem ligacao, > 1 = loop, 0 = sem ligacao
+    private boolean ehDirigido = false;
+
+    public String tipoDoGrafo(int[][] matrizAdjacencia) {
         int numElementos = Math.abs(this.getNumeroElementos(matrizAdjacencia));
         int contNulo = 0;
         int somaDiagonalPrincipal = 0;
         boolean ehMultigrafo = false;
-        boolean ehDirigido = false;
         boolean ehCompleto = false;
         boolean ehRegular = false;
+        boolean temLoop = false;
         boolean ehBipartido = false;
         String msg = "O tipo do grafo inserido seria: ";
 
@@ -27,24 +32,41 @@ public class AnalisaGrafo {
                     ehMultigrafo = true;
                 } else if (i == j) {
                     somaDiagonalPrincipal += matrizAdjacencia[i][j];
-                    if (somaDiagonalPrincipal > 0) ehMultigrafo = true;
+                    if (somaDiagonalPrincipal > 0) ehMultigrafo = true; temLoop = true;
                 }
 
                 // verificando dirigido / nao-dirigido & completo
                 if (!ehMultigrafo) {
-                    if (!(matrizAdjacencia[i][j] == matrizAdjacencia[j][i])) {
+                    if (!this.verifSimetria(matrizAdjacencia, i, j)) {
                         ehDirigido = true;
                     }
-                    ehCompleto = true;
+
+                    // verificando completo
+                    if (contNulo == 1 && this.verifNulo(contNulo, numElementos)) { // cobre o caso do K1, unico nulo completo
+                        ehCompleto = true;
+                    } else {
+
+                    }
                 } else {
-                    //...
+                    if (temLoop) {
+                        ehDirigido = true;
+                    } else if (!temLoop && this.verifSimetria(matrizAdjacencia, i, j)) {
+                        ehDirigido = false;
+                    }
+                }
+
+                // verificando bipartido
+                if (!ehMultigrafo) {
+
+                } else if (ehMultigrafo && !temLoop) {
+
                 }
             }
         }
 
         // montando a mensagem baseado nas afirmacoes
 
-        if (contNulo == numElementos) msg += "Nulo - "; ehRegular = true;
+        if (this.verifNulo(contNulo, numElementos)) msg += "Nulo - "; ehRegular = true;
 
         if (ehMultigrafo) {
             msg += "Multigrafo - ";
@@ -62,9 +84,18 @@ public class AnalisaGrafo {
             msg += "Completo - ";
             ehRegular = true;
         }
-
+        if (ehBipartido) msg += "Bipartido - ";
         if (ehRegular) msg += "Regular - ";
+
         return this.arrumaMsg(msg);
+    }
+
+    private boolean verifSimetria(int[][] matrizAdjacente, int i, int j) {
+        return matrizAdjacente[i][j] == matrizAdjacente[j][i];
+    }
+
+    private boolean verifNulo(int contNulo, int numElementos) {
+        return contNulo == numElementos;
     }
 
     private String arrumaMsg(String msg) {
@@ -77,25 +108,32 @@ public class AnalisaGrafo {
         int numElementos = 0;
 
         for (int i = 0; i < Array.getLength(object); i++) {
-            numElementos -= getNumeroElementos(Array.get(object, i));
+            numElementos += getNumeroElementos(Array.get(object, i));
         }
         return numElementos;
     }
 
     public String arestasDoGrafo(int[][] matrizAdjacencia) {
         String msg = "";
-        int contArestas = 0;
+        int somaArestas = 0;
+        int numArestas = 0;
 
+        // solucao p/ nao dirigidos e dirigidos (quantidade de arestas)
         for (int i = 0; i < matrizAdjacencia.length; i++) {
             for (int j = 0; j < matrizAdjacencia.length; j++) {
+                // contando as arestas
                 if (matrizAdjacencia[i][j] > 0) {
-                    contArestas++;
+                    somaArestas += matrizAdjacencia[i][j];
                 }
+
+                // conexoes
+
             }
         }
+        if (!ehDirigido) numArestas = somaArestas / 2; else numArestas = somaArestas;
 
-        if (contArestas > 0) {
-            msg += "Numero de arestas existentes no grafo: " + contArestas;
+        if (numArestas > 0) {
+            msg += "Numero de arestas existentes no grafo: " + numArestas;
         } else {
             msg += "Este grafo nao possui arestas.";
         }
@@ -109,10 +147,13 @@ public class AnalisaGrafo {
     public static void main(String[] args) {
         AnalisaGrafo obj = new AnalisaGrafo();
         int[][] matrizAdjacencia = {
-                {0},
+                {0, 1, 1, 1},
+                {1, 0, 1, 1},
+                {1, 1, 0, 1},
+                {1, 1, 1, 0}
         };
 
         System.out.println(obj.tipoDoGrafo(matrizAdjacencia));
-        //System.out.println(obj.arestasDoGrafo(matrizAdjacencia));
+        System.out.println(obj.arestasDoGrafo(matrizAdjacencia));
     }
 }
